87276eabd98d6d860a38e795f47a6d452c0cb017
Tomer <tomerhadad@pingidentity.com>
2023-06-14 16:26:34 +0300

    version 5.1.1

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PingOneSignals",
-            url: "https://assets.pingone.com/signals/ios-sdk/5.1.0/PingOneSignals.xcframework.zip",
-            checksum: "4df5c82d55421abe55ea30b92f741e3515d93e1a884e0890c87908f3ee122909"),
+            url: "https://assets.pingone.com/signals/ios-sdk/5.1.1/PingOneSignals.xcframework.zip",
+            checksum: "c2d30c00f404b5c0d0f0cad35c736ef43ca62de8df61d913c45321aba94fc72a"),
     ]
 )
