59fde21ecebaaf1b20046e79f55f05f4beb7a74d
yizharkatif <138602732+yizharkatif@users.noreply.github.com>
2024-05-21 20:34:25 +0300

    Update Package.swift
    Increase MinimumOSVersion to large number in order to override invalid bundle Apple's issue.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PingOneSignals",
-            url: "https://assets.pingone.com/signals/ios-sdk/5.2.6/PingOneSignals.xcframework.zip",
-            checksum: "6a7b5f67edeee1f96e439bee68d91facd47cfb75ac9ccb04d20d55b157a2477e"),
+            url: "https://assets.pingone.com/signals/ios-sdk/5.2.7/PingOneSignals.xcframework.zip",
+            checksum: "acd1463a2f6ffe89f01e5229715a29944cb5cdc09dd9f5d8cffc7ab5ffeb7a6d"),
     ]
 )
