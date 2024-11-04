2f47da5704ed7bacc3eb507db2f3a4ecc873c05d
yizharkatif <138602732+yizharkatif@users.noreply.github.com>
2024-11-04 11:38:23 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PingOneSignals",
-            url: "https://assets.pingone.com/signals/ios-sdk/5.2.7/PingOneSignals.xcframework.zip",
-            checksum: "acd1463a2f6ffe89f01e5229715a29944cb5cdc09dd9f5d8cffc7ab5ffeb7a6d"),
+            url: "https://assets.pingone.com/signals/ios-sdk/5.2.8/PingOneSignals.xcframework.zip",
+            checksum: "0943a4950d33876448bce52f927d9a83486c56bc309a02d348d7dbbbed6b8d04"),
     ]
 )
